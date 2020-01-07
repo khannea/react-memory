3fe20058155d64d1eacad7c22214fd0dc4c6174e
unknown <khanhigoufake2@gmail.com>
2020-01-08 00:59:33 +0100

    nom change en name

--- frontend/src/HallOfFame.js
+++ frontend/src/HallOfFame.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-
-import './HallOfFame.css'
+import "./HallOfFame.css";
 
 const HallOfFame = ({ entries }) => (
   <table className="hallOfFame">
     <tbody>
-    {
-      entries.map(({ nom, guesses, id}) => (
+      {entries.map(({ nom, guesses, id }) => (
         <tr key={id}>
-          <td className="nom">{nom}</td>
+          <td className="name">{name}</td>
           <td className="guesses">{guesses}</td>
         </tr>
-      ))
-    }
+      ))}
     </tbody>
   </table>
-)
+);
 
-export default HallOfFame
+export default HallOfFame;
 
 // == Internal helpers ==============================================
 
@@ -30,38 +27,37 @@ export default HallOfFame
 //   { id: 0, guesses: 48, date: '14/10/2017', player: 'Marc' },
 // ]
 
-
 HallOfFame.propTypes = {
   entries: PropTypes.arrayOf(
     PropTypes.shape({
       guesses: PropTypes.string.isRequired,
       id: PropTypes.string.isRequired,
-      nom: PropTypes.string.isRequired,
+      nom: PropTypes.string.isRequired
     })
-  ).isRequired,
-}
+  ).isRequired
+};
 
-const HOF_KEY = '::Memory::HallofFame'
-const HOF_MAX_SIZE = 10
+const HOF_KEY = "::Memory::HallofFame";
+const HOF_MAX_SIZE = 10;
 
 export function saveHOFEntry(entry, onStored) {
-  entry.date = new Date().toLocaleDateString()
-  entry.id = Date.now()
+  entry.date = new Date().toLocaleDateString();
+  entry.id = Date.now();
 
-  const entries = JSON.parse(localStorage.getItem(HOF_KEY) || '[]')
+  const entries = JSON.parse(localStorage.getItem(HOF_KEY) || "[]");
   const insertionPoint = entries.findIndex(
     ({ guesses }) => guesses >= entry.guesses
-  )
+  );
 
   if (insertionPoint === -1) {
-    entries.push(entry)
+    entries.push(entry);
   } else {
-    entries.splice(insertionPoint, 0, entry)
+    entries.splice(insertionPoint, 0, entry);
   }
   if (entries.length > HOF_MAX_SIZE) {
-    entries.splice(HOF_MAX_SIZE, entries.length)
+    entries.splice(HOF_MAX_SIZE, entries.length);
   }
 
-  localStorage.setItem(HOF_KEY, JSON.stringify(entries))
-  onStored(entries)
+  localStorage.setItem(HOF_KEY, JSON.stringify(entries));
+  onStored(entries);
 }
