879524401a89189865ea141babf756702ea9bebd
unknown <khanhigoufake2@gmail.com>
2020-03-21 23:05:53 +0100

    remove double

--- backend/server.js
+++ backend/server.js
@@ -96,7 +96,7 @@ router.route("/tetris_rank/add").post((req, res) => {
 
 router.route("/corona/addvote").post((req, res) => {
   let item = new Vote(req.body);
-  Vote.delete({ from: item.from });
+  Vote.deleteMany({ from: item.from });
   item
     .save()
     .then(item => {
